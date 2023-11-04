fb9d4581b8a28e1eb07dbf84b920bf3b089cbfeb
Luis Ramirez <laketurtle87@gmail.com>
2023-11-03 21:56:16 -0700

    Initial Commit

--- 2_ForeignKeyCreation.sql
+++ 2_ForeignKeyCreation.sql
@@ -17,4 +17,3 @@ ALTER TABLE `Pizza_Orders` ADD FOREIGN KEY (`pizza_id`) REFERENCES `Pizza_Menu` 
 
 
 
-Successfully created project 'Assignment12' on GitHub, but initial commit failed: Author identity unknown *** Please tell me who you are. Run git config --global user.email "you@example.com" git config --global user.name "Your Name" to set your account's default identity. Omit --global to set the identity only in this repository. unable to auto-detect email address (got 'nyusu@DESKTOP-T6R0NM0.(none)')
